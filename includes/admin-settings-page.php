8d4d16e3a727d98d9c54dc7c7963f338b7bdfed7
unclemusclez <unclemusclez@gmail.com>
2025-04-02 20:17:02 -0400

    indent

--- includes/admin-settings-page.php
+++ includes/admin-settings-page.php
@@ -120,80 +120,80 @@ function git_activity_charts_settings_page_html($plugin) {
             <?php submit_button(); ?>
         </form>
     </div>
-    <div id="account-template" style="display: none;">
-        <div class="account-group" data-index="__INDEX__">
-            <div class="account-header">
-                <h4><?php _e('Account __INDEX__', 'git-activity-charts'); ?> <span class="account-type-indicator">GitHub</span></h4>
-                <button type="button" class="button button-link-delete remove-account"><?php _e('Remove', 'git-activity-charts'); ?></button>
-            </div>
-            <table class="form-table">
-                <tr>
-                    <th scope="row"><label><?php _e('Provider', 'git-activity-charts'); ?></label></th>
-                    <td>
-                        <select name="git_activity_accounts[__INDEX__][type]" class="account-type-select">
-                            <option value="github">GitHub</option>
-                            <option value="gitlab">GitLab</option>
-                            <option value="gitea">Gitea</option>
-                            <option value="bitbucket">Bitbucket</option>
-                            <option value="custom">Custom</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row"><label><?php _e('Username', 'git-activity-charts'); ?></label></th>
-                    <td><input type="text" name="git_activity_accounts[__INDEX__][username]" placeholder="<?php _e('Your username on the platform', 'git-activity-charts'); ?>" required class="regular-text" /></td>
-                </tr>
-                <tr>
-                    <th scope="row"><label><?php _e('API Key/Token', 'git-activity-charts'); ?></label></th>
-                    <td>
-                        <div style="position: relative;">
-                            <input type="password" name="git_activity_accounts[__INDEX__][api_key]" placeholder="<?php _e('Personal Access Token (optional)', 'git-activity-charts'); ?>" class="regular-text api-key-input" autocomplete="new-password"/>
-                            <button type="button" class="button button-secondary toggle-api-key" style="position: absolute; right: 1px; top: 1px; height: 28px; margin:0;"><?php _e('Show', 'git-activity-charts'); ?></button>
-                        </div>
-                        <p class="description"><?php _e('Needed for private repos or to increase rate limits.', 'git-activity-charts'); ?></p>
-                    </td>
-                </tr>
-                <tr class="account-field repos-field">
-                    <th scope="row"><label><?php _e('Repositories', 'git-activity-charts'); ?></label></th>
-                    <td>
-                        <input type="text" name="git_activity_accounts[__INDEX__][repos]" placeholder="<?php _e('repo1, repo2, another-repo', 'git-activity-charts'); ?>" class="large-text" />
-                        <p class="description"><?php _e('Comma-separated list of repository names (required for GitLab, Gitea, Bitbucket).', 'git-activity-charts'); ?></p>
-                    </td>
-                </tr>
-                <tr class="account-field instance-url-field">
-                    <th scope="row"><label><?php _e('Instance URL', 'git-activity-charts'); ?></label></th>
-                    <td>
-                        <input type="url" name="git_activity_accounts[__INDEX__][instance_url]" placeholder="<?php _e('e.g., https://saltrivercanyon.com', 'git-activity-charts'); ?>" class="regular-text"/>
-                        <p class="description"><?php _e('Required for self-hosted GitLab, Gitea, or custom instances.', 'git-activity-charts'); ?></p>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row"><?php _e('Display Options', 'git-activity-charts'); ?></th>
-                    <td>
-                        <fieldset>
-                            <label>
-                                <input type="checkbox" name="git_activity_accounts[__INDEX__][use_color_logo]" value="1" />
-                                <?php _e('Use Color Logo (if available)', 'git-activity-charts'); ?>
-                            </label>
-                            <br>
-                            <label style="margin-top: 10px; display: inline-block;">
-                                <?php _e('Title Text Color:', 'git-activity-charts'); ?>
-                                <input type="text" name="git_activity_accounts[__INDEX__][text_color]" class="color-picker" data-default-color="#000000" />
-                            </label>
-                            <br>
-                            <label style="margin-top: 10px; display: inline-block;">
-                                <?php _e('Custom Logo:', 'git-activity-charts'); ?>
-                                <input type="text" name="git_activity_accounts[__INDEX__][custom_logo]" value="" class="custom-logo-url regular-text" readonly />
-                                <input type="button" class="button custom-logo-upload-button" value="<?php _e('Upload/Select Logo', 'git-activity-charts'); ?>" />
-                                <input type="button" class="button custom-logo-remove-button" value="<?php _e('Remove Logo', 'git-activity-charts'); ?>" style="display:none;" />
-                                <p class="description"><?php _e('Upload or select a logo from the media library. Overrides the default provider logo.', 'git-activity-charts'); ?></p>
-                            </label>
-                        </fieldset>
-                    </td>
-                </tr>
-            </table>
-            <hr />
+<div id="account-template" style="display: none;">
+    <div class="account-group" data-index="__INDEX__">
+        <div class="account-header">
+            <h4><?php _e('Account __INDEX__', 'git-activity-charts'); ?> <span class="account-type-indicator">GitHub</span></h4>
+            <button type="button" class="button button-link-delete remove-account"><?php _e('Remove', 'git-activity-charts'); ?></button>
         </div>
+        <table class="form-table">
+            <tr>
+                <th scope="row"><label><?php _e('Provider', 'git-activity-charts'); ?></label></th>
+                <td>
+                    <select name="git_activity_accounts[__INDEX__][type]" class="account-type-select">
+                        <option value="github">GitHub</option>
+                        <option value="gitlab">GitLab</option>
+                        <option value="gitea">Gitea</option>
+                        <option value="bitbucket">Bitbucket</option>
+                        <option value="custom">Custom</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row"><label><?php _e('Username', 'git-activity-charts'); ?></label></th>
+                <td><input type="text" name="git_activity_accounts[__INDEX__][username]" placeholder="<?php _e('Your username on the platform', 'git-activity-charts'); ?>" required class="regular-text" /></td>
+            </tr>
+            <tr>
+                <th scope="row"><label><?php _e('API Key/Token', 'git-activity-charts'); ?></label></th>
+                <td>
+                    <div style="position: relative;">
+                        <input type="password" name="git_activity_accounts[__INDEX__][api_key]" placeholder="<?php _e('Personal Access Token (optional)', 'git-activity-charts'); ?>" class="regular-text api-key-input" autocomplete="new-password"/>
+                        <button type="button" class="button button-secondary toggle-api-key" style="position: absolute; right: 1px; top: 1px; height: 28px; margin:0;"><?php _e('Show', 'git-activity-charts'); ?></button>
+                    </div>
+                    <p class="description"><?php _e('Needed for private repos or to increase rate limits.', 'git-activity-charts'); ?></p>
+                </td>
+            </tr>
+            <tr class="account-field repos-field">
+                <th scope="row"><label><?php _e('Repositories', 'git-activity-charts'); ?></label></th>
+                <td>
+                    <input type="text" name="git_activity_accounts[__INDEX__][repos]" placeholder="<?php _e('repo1, repo2, another-repo', 'git-activity-charts'); ?>" class="large-text" />
+                    <p class="description"><?php _e('Comma-separated list of repository names (required for GitLab, Gitea, Bitbucket).', 'git-activity-charts'); ?></p>
+                </td>
+            </tr>
+            <tr class="account-field instance-url-field">
+                <th scope="row"><label><?php _e('Instance URL', 'git-activity-charts'); ?></label></th>
+                <td>
+                    <input type="url" name="git_activity_accounts[__INDEX__][instance_url]" placeholder="<?php _e('e.g., https://saltrivercanyon.com', 'git-activity-charts'); ?>" class="regular-text"/>
+                    <p class="description"><?php _e('Required for self-hosted GitLab, Gitea, or custom instances.', 'git-activity-charts'); ?></p>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row"><?php _e('Display Options', 'git-activity-charts'); ?></th>
+                <td>
+                    <fieldset>
+                        <label>
+                            <input type="checkbox" name="git_activity_accounts[__INDEX__][use_color_logo]" value="1" />
+                            <?php _e('Use Color Logo (if available)', 'git-activity-charts'); ?>
+                        </label>
+                        <br>
+                        <label style="margin-top: 10px; display: inline-block;">
+                            <?php _e('Title Text Color:', 'git-activity-charts'); ?>
+                            <input type="text" name="git_activity_accounts[__INDEX__][text_color]" class="color-picker" data-default-color="#000000" />
+                        </label>
+                        <br>
+                        <label style="margin-top: 10px; display: inline-block;">
+                            <?php _e('Custom Logo:', 'git-activity-charts'); ?>
+                            <input type="text" name="git_activity_accounts[__INDEX__][custom_logo]" value="" class="custom-logo-url regular-text" readonly />
+                            <input type="button" class="button custom-logo-upload-button" value="<?php _e('Upload/Select Logo', 'git-activity-charts'); ?>" />
+                            <input type="button" class="button custom-logo-remove-button" value="<?php _e('Remove Logo', 'git-activity-charts'); ?>" style="display:none;" />
+                            <p class="description"><?php _e('Upload or select a logo from the media library. Overrides the default provider logo.', 'git-activity-charts'); ?></p>
+                        </label>
+                    </fieldset>
+                </td>
+            </tr>
+        </table>
+        <hr />
     </div>
-    <?php
+</div>
+<?php
 }
